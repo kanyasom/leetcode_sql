62271d8ffe53e7963096932d716e8cc2c6d947e3
kanyasom <somkanya.kankanbhowmik@gmail.com>
2024-12-13 20:17:42 +0530

    sql added

---
+++ premium/evaluate-boolean-expression.sql
@@ -0,0 +1,88 @@
+--MEDIUM
+/*
+Table Variables:
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| name          | varchar |
+| value         | int     |
++---------------+---------+
+In SQL, name is the primary key for this table.
+This table contains the stored variables and their values.
+ 
+
+Table Expressions:
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| left_operand  | varchar |
+| operator      | enum    |
+| right_operand | varchar |
++---------------+---------+
+In SQL, (left_operand, operator, right_operand) is the primary key for this table.
+This table contains a boolean expression that should be evaluated.
+operator is an enum that takes one of the values ('<', '>', '=')
+The values of left_operand and right_operand are guaranteed to be in the Variables table.
+ 
+
+Evaluate the boolean expressions in Expressions table.
+
+Return the result table in any order.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Variables table:
++------+-------+
+| name | value |
++------+-------+
+| x    | 66    |
+| y    | 77    |
++------+-------+
+Expressions table:
++--------------+----------+---------------+
+| left_operand | operator | right_operand |
++--------------+----------+---------------+
+| x            | >        | y             |
+| x            | <        | y             |
+| x            | =        | y             |
+| y            | >        | x             |
+| y            | <        | x             |
+| x            | =        | x             |
++--------------+----------+---------------+
+Output: 
++--------------+----------+---------------+-------+
+| left_operand | operator | right_operand | value |
++--------------+----------+---------------+-------+
+| x            | >        | y             | false |
+| x            | <        | y             | true  |
+| x            | =        | y             | false |
+| y            | >        | x             | true  |
+| y            | <        | x             | false |
+| x            | =        | x             | true  |
++--------------+----------+---------------+-------+
+Explanation: 
+As shown, you need to find the value of each boolean expression in the table using the variables table.
+*/
+
+
+--ANSWER
+SELECT e.left_operand,e.operator,e.right_operand,
+(
+CASE 
+    WHEN e.operator = '>' AND v1.value > v2.value THEN 'true'
+    WHEN e.operator = '<' AND v1.value < v2.value THEN 'true'
+    WHEN e.operator = '=' AND v1.value = v2.value THEN 'true'
+else 
+   'false'
+END
+)AS value
+FROM Expressions e
+join Variables v1 ON v1.name = e.left_operand
+join Variables v2 ON v2.name = e.right_operand
--- premium/grand-slam-titles.sql
+++ premium/grand-slam-titles.sql
@@ -67,3 +67,40 @@ Player 3 (Novak) did not win anything, we did not include them in the result tab
 */
 
 --answer
+--APPROACH 1 : using SUM and JOIN
+SELECT player_id,player_name,
+SUM(player_id=Wimbledon)+SUM(player_id=Fr_open)+SUM(player_id=US_open)+SUM(player_id=Au_open)
+as grand_slams_count
+FROM Players
+JOIN Championships
+ON player_id=Wimbledon or player_id=Fr_open or player_id=US_open or player_id=Au_open
+GROUP BY player_id;
+
+--APPROACH 2 : CASE WHEN
+SELECT * 
+FROM (
+  SELECT 
+   player_id,
+   player_name,
+   SUM( CASE WHEN player_id = Wimbledon THEN 1 ELSE 0 END +
+        CASE WHEN player_id = Fr_open THEN 1 ELSE 0 END +
+        CASE WHEN player_id = US_open THEN 1 ELSE 0 END +
+        CASE WHEN player_id = Au_open THEN 1 ELSE 0 END ) AS grand_slams_count
+  FROM Players CROSS JOIN Championships GROUP BY player_id, player_name ) T
+WHERE grand_slams_count > 0
+
+--APPROACH 3 : 
+WITH CTE AS 
+   (
+    SELECT Wimbledon AS id FROM Championships
+    UNION ALL 
+    SELECT Fr_open AS id FROM Championships
+    UNION ALL 
+    SELECT US_open AS id FROM Championships
+    UNION ALL 
+    SELECT Au_open AS id FROM Championships 
+   )
+   
+SELECT player_id,player_name,COUNT(*) AS grand_slams_count
+FROM Players INNER JOIN CTE ON Players.player_id = CTE.id
+GROUP BY player_id,player_name
